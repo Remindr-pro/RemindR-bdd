9a7807c47af59ac179ddbc29c6abe944b56e45b6
Clément Daguenet <99010993+ClementDaguenet@users.noreply.github.com>
2026-01-16 15:14:33 +0100

    CI/CD Tests

--- src/config/passport.ts
+++ src/config/passport.ts
@@ -6,56 +6,58 @@ import { UserType, User } from '@prisma/client';
 
 export type PassportUser = Pick<User, 'id' | 'email' | 'firstName' | 'lastName' | 'role' | 'userType' | 'familyId' | 'isActive'>;
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: oauth2Config.google.clientId,
-      clientSecret: oauth2Config.google.clientSecret,
-      callbackURL: oauth2Config.google.redirectUri,
-    },
-    async (_accessToken, _refreshToken, profile, done) => {
-      try {
-        if (!profile.emails || !profile.emails[0]) {
-          return done(new Error('No email provided by Google'), undefined);
-        }
-
-        const email = profile.emails[0].value;
-        const firstName = profile.name?.givenName || 'User';
-        const lastName = profile.name?.familyName || '';
-        const profilePictureUrl = profile.photos?.[0]?.value;
-
-        let user = await prisma.user.findUnique({
-          where: { email },
-        });
-
-        if (!user) {
-          user = await prisma.user.create({
-            data: {
-              email,
-              passwordHash: '', // OAuth users don't have passwords
-              firstName,
-              lastName,
-              dateOfBirth: new Date('1990-01-01'), // Default, should be updated
-              userType: UserType.INDIVIDUAL,
-              profilePictureUrl,
-            },
-          });
-        } else {
-          if (profilePictureUrl && !user.profilePictureUrl) {
-            user = await prisma.user.update({
-              where: { id: user.id },
-              data: { profilePictureUrl },
-            });
+if (oauth2Config.google.clientId && oauth2Config.google.clientSecret && oauth2Config.google.redirectUri) {
+  passport.use(
+    new GoogleStrategy(
+      {
+        clientID: oauth2Config.google.clientId,
+        clientSecret: oauth2Config.google.clientSecret,
+        callbackURL: oauth2Config.google.redirectUri,
+      },
+      async (_accessToken, _refreshToken, profile, done) => {
+        try {
+          if (!profile.emails || !profile.emails[0]) {
+            return done(new Error('No email provided by Google'), undefined);
           }
-        }
 
-        return done(null, user as PassportUser);
-      } catch (error) {
-        return done(error, undefined);
+          const email = profile.emails[0].value;
+          const firstName = profile.name?.givenName || 'User';
+          const lastName = profile.name?.familyName || '';
+          const profilePictureUrl = profile.photos?.[0]?.value;
+
+          let user = await prisma.user.findUnique({
+            where: { email },
+          });
+
+          if (!user) {
+            user = await prisma.user.create({
+              data: {
+                email,
+                passwordHash: '', // OAuth users don't have passwords
+                firstName,
+                lastName,
+                dateOfBirth: new Date('1990-01-01'), // Default, should be updated
+                userType: UserType.INDIVIDUAL,
+                profilePictureUrl,
+              },
+            });
+          } else {
+            if (profilePictureUrl && !user.profilePictureUrl) {
+              user = await prisma.user.update({
+                where: { id: user.id },
+                data: { profilePictureUrl },
+              });
+            }
+          }
+
+          return done(null, user as PassportUser);
+        } catch (error) {
+          return done(error, undefined);
+        }
       }
-    }
-  )
-);
+    )
+  );
+}
 
 passport.serializeUser((user: any, done) => {
   done(null, user.id);
